24ed293142402fdc4ea231f0e267be30efea42d1
Rocio Rios <rrocio46@yahoo.es>
2020-12-12 13:24:50 -0600

    adding the home page

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import HomePage from './home-page.component';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return <div className='App'><HomePage /></div>;
+  }
+}
+
+export default App;
